59d400465b10f4ba4c20bca270b788412f6928a5
윤혜경 <sunrespect@naver.com>
2024-01-17 22:48:55 +0900

    Add: Solve beakjoon 22945 팀 빌딩

---
+++ hyetae/3주차/22945 팀 빌딩.cpp
@@ -0,0 +1,34 @@
+//
+// Created by 윤혜경 on 1/17/24.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    vector<int> v(N);
+    for (int i = 0; i < N; i++) {
+        cin >> v[i];
+    }
+
+    int maxi = 0;
+    int pt1 = 0, pt2 = N - 1;
+    while (pt1 < pt2) {
+        maxi = max(maxi, (pt2 - pt1 - 1) * min(v[pt1], v[pt2]));
+
+        if (v[pt1] < v[pt2])
+            pt1++;
+        else
+            pt2--;
+    }
+
+    cout << maxi;
+
+    return 0;
+}
