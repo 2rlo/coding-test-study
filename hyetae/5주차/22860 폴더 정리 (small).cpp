d283b0d1b8a0be8fbd30decda36d70b698fc9d6f
hyetae <86393948+hyetae@users.noreply.github.com>
2024-04-08 22:07:29 +0900

    Add: Solve beackjoon 22860 폴더 정리 (small)

--- hyetae/5주차/22860 폴더 정리 (small).cpp
+++ hyetae/5주차/22860 폴더 정리 (small).cpp
@@ -5,50 +5,62 @@
 #define fastio cin.tie(0)->sync_with_stdio(0)
 #define MAX 1001
 using namespace std;
-map<string, pair<int, int>> idx;
-int graph[MAX][MAX] = { 0, };
 
-pair<int, int> dfs(string goal){
+int cnt = 0;
+int N, M, C, Q;
+string P, F, q;
+map<string, vector<pair<string, int>>> tree;
+set<string> s;
 
+void dfs(string target, string now) {
+    for (int i = 0; i < tree[now].size(); i++) {
+        if (!tree[now][i].second) {
+            s.insert(tree[now][i].first);
+            cnt++;
+        } else dfs(target, tree[now][i].first);
+    }
+
+    if (target != now)
+        return;
+
+    cout << s.size() << ' ' << cnt << '\n';
+    cnt = 0;
+    s.clear();
+    return;
+}
+
+string parser(string q) {
+    string name = "";
+    for (char c: q) {
+        if (c == '/')
+            name = "";
+        else
+            name += c;
+    }
+    return name;
 }
 
 int main() {
     fastio;
-
-    int N, M;
     cin >> N >> M;
 
-    int C, index = 0;
-    string P, F;
     for (int i = 0; i < N + M; i++) {
         cin >> P >> F >> C;
-        if (P == "main")
-            idx[P] = make_pair(index++, 1);
-        else if (!idx.count(P))
-            idx[P] = make_pair(index++, C);
-        else if (!idx.count(F))
-            idx[F] = make_pair(index++, C);
-
-        graph[idx[P].first][idx[F].first] = 1;
-    }
-
-    int Q;
-    cin >> Q;
-    for (int i = 0; i < Q; i++) {
-        string input;
-        cin >> input;
-        string goal;
-        for (char c: input) {
-            if (isalpha(c))
-                goal += c;
-            else
-                goal = "";
+        if (tree.find(P) == tree.end()) {
+            vector<pair<string, int>> v;
+            v.push_back({F, C});
+            tree[P] = v;
+        } else {
+            tree[P].push_back({F, C});
         }
-
-        pair<int, int> res = dfs(goal);
-        cout << res.first << ' ' << res.second << '\n';
     }
 
+    cin >> Q;
+    while (Q--) {
+        cin >> q;
+        string target = parser(q);
+        dfs(target, target);
+    }
 
-
+    return 0;
 }
